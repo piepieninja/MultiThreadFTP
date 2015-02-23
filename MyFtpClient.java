1931df947c01d91def4946838428a381b5620ddf
Jaicob <jaicob@icloud.com>
2015-02-23 16:09:32 -0500

    more clien refactoring also need to fix how commands are passed

--- MyFtpClient.java
+++ MyFtpClient.java
@@ -6,40 +6,119 @@ public class MyFtpClient {
 
 	private String IP, command;
 	private int nport, tport;
+	private static Socket normalSocket;	
+	private static Socket terminateSocket;
+	private static BufferedReader normalIn;
+	private static BufferedReader terminateIn;
+	private static PrintWriter normalOut;
+	private static PrintWriter terminateOut;
+    private static BufferedReader stdIn;
 
 	public MyFtpClient(String IP, int nport, int tport){
 		this.IP = IP;
 		this.nport = nport;
 		this.tport = tport;
+		beginCommunication();
+	}
 
-		try (
-			//creates a socket to connect to the client port on the server
-			Socket normalSocket = new Socket(IP, nport);
-			
-			//creates a socket to connect to the terminate port on the server
-			Socket terminateSocket = new Socket(IP, tport);
+	public void sendCommand(String userInput ) throws Exception {
+		String data;
+		if (command.equals("put")) {
+			File file = new File(System.getProperty("user.dir") + "/" + userInput.substring(userInput.indexOf(' ') + 1));
+			System.out.println(System.getProperty("user.dir") + "/" + userInput.substring(userInput.indexOf(' ') + 1));
+			//Check if directory exists
+			if (!file.exists()) {
+				System.out.println("ERROR: That file does not exist");
+			} else if (file.exists()) {
+				// add stuff and things
+				byte [] fileByteArray  = new byte [(int)file.length()];
+				FileInputStream fis = new FileInputStream(file);
+				BufferedInputStream bis = new BufferedInputStream(fis);
+				bis.read(fileByteArray,0,fileByteArray.length);
+				OutputStream os = normalSocket.getOutputStream();
+				os.write(fileByteArray,0,fileByteArray.length);
+					os.flush();
+				fis.close();
+				bis.close();
+			} else {
+				System.out.println("ERROR: That is not a file.");
+			}
+		} else if (command.equals("get")) {
+			System.out.println("Executing get command client side");
+			normalOut.println(userInput);
+			System.out.println(normalIn.readLine());
+		} else {
+			normalOut.println(userInput);
+	    	if (command.equals("pwd")) {
+				//prints out the current directory
+				System.out.println(normalIn.readLine());
 
-			//Input for each socket
-			BufferedReader normalIn = 
-				new BufferedReader(
-					new InputStreamReader(normalSocket.getInputStream()));
-			BufferedReader terminateIn = 
-				new BufferedReader(
-					new InputStreamReader(terminateSocket.getInputStream()));
+			} else if (command.equals("ls")) {
+				//prints out all of the files in the current directory
+				data = normalIn.readLine();
+				
+				//check if there are no files or directories in current directory
+				if (data.equals("")) {
+					System.out.print("");
+				} else {
+					data = data.replace("<&&newline&&>", "\n");
+					System.out.println(data.substring(0, data.length() - 1));
+				}
 
-			//Output for each socket
-			PrintWriter normalOut =
-                new PrintWriter(normalSocket.getOutputStream(), true);
-        	PrintWriter terminateOut =
-                new PrintWriter(terminateSocket.getOutputStream(), true);
+			} else if (command.equals("cd")) {
+				//changes the directory to the specified directory
+				data = normalIn.readLine();
+				if (data.equals("no directory")) {
+					System.out.println("ERROR: Directory does not exist");
+				} else if (data.equals("not a directory")) {
+					System.out.println("ERROR: That is not a directory");
+				}
 
-            //Input from the user
-            BufferedReader stdIn =
-                new BufferedReader(
-                    new InputStreamReader(System.in))
+			} else if (command.equals("mkdir")) {
+				//creates a directory in the current directory
+				data = normalIn.readLine();
+				if (data.equals("failure")) {
+					System.out.println("ERROR: Cannot create directory");
+				} 
+			} else if (command.equals("delete")) {
+				//deletes the specified file
+				data = normalIn.readLine();
+				if (data.equals("no file")) {
+					System.out.println("ERROR: File does not exist");
+				} else if (data.equals("failure")) {
+					System.out.println("ERROR: That is not a file");
+				}
+			} else if (command.equals("quit")) {
+				//close all streams and exit
+				normalIn.close();
+				terminateIn.close();
+				normalOut.close();
+				terminateOut.close();
+				stdIn.close();
+				System.exit(0);
+			}
+		}
+	}
 
-		) {
-			String userInput, data;
+	private void setupSocket() throws Exception{
+		//creates a socket to connect to the client port on the server
+		 normalSocket = new Socket(IP, nport);	
+		//creates a socket to connect to the terminate port on the server
+		terminateSocket = new Socket(IP, tport);
+		//Input for each socket
+		 normalIn = new BufferedReader( new InputStreamReader(normalSocket.getInputStream()));
+		 terminateIn = new BufferedReader(new InputStreamReader(terminateSocket.getInputStream()));
+		//Output for each socket
+		 normalOut = new PrintWriter(normalSocket.getOutputStream(), true);
+    	 terminateOut = new PrintWriter(terminateSocket.getOutputStream(), true);
+        //Input from the user
+         stdIn = new BufferedReader(new InputStreamReader(System.in));
+	}
+
+	public void beginCommunication(){
+		try {
+			setupSocket();
+			String userInput;
 			short sh;
 			while(true) {
 				System.out.print("myftp> ");
@@ -53,83 +132,9 @@ public class MyFtpClient {
 						terminateOut.println("terminate");
 						break;
 					case 1:
-						if (command.equals("put")) {
-							File file = new File(System.getProperty("user.dir") + "/" + userInput.substring(userInput.indexOf(' ') + 1));
-							System.out.println(System.getProperty("user.dir") + "/" + userInput.substring(userInput.indexOf(' ') + 1));
-							//Check if directory exists
-							if (!file.exists()) {
-								System.out.println("ERROR: That file does not exist");
-							} else if (file.exists()) {
-								// add stuff and things
-								byte [] fileByteArray  = new byte [(int)file.length()];
-								FileInputStream fis = new FileInputStream(file);
-								BufferedInputStream bis = new BufferedInputStream(fis);
-								bis.read(fileByteArray,0,fileByteArray.length);
-								OutputStream os = normalSocket.getOutputStream();
-								os.write(fileByteArray,0,fileByteArray.length);
-		      					os.flush();
-    							fis.close();
-    							bis.close();
-							} else {
-								System.out.println("ERROR: That is not a file.");
-							}
-						} else if (command.equals("get")) {
-							System.out.println("Executing get command client side");
-							normalOut.println(userInput);
-							System.out.println(normalIn.readLine());
-						} else {
-								normalOut.println(userInput);
-						    	if (command.equals("pwd")) {
-									//prints out the current directory
-									System.out.println(normalIn.readLine());
-
-								} else if (command.equals("ls")) {
-									//prints out all of the files in the current directory
-									data = normalIn.readLine();
-									
-									//check if there are no files or directories in current directory
-									if (data.equals("")) {
-										System.out.print("");
-									} else {
-										data = data.replace("<&&newline&&>", "\n");
-										System.out.println(data.substring(0, data.length() - 1));
-									}
-
-								} else if (command.equals("cd")) {
-									//changes the directory to the specified directory
-									data = normalIn.readLine();
-									if (data.equals("no directory")) {
-										System.out.println("ERROR: Directory does not exist");
-									} else if (data.equals("not a directory")) {
-										System.out.println("ERROR: That is not a directory");
-									}
-
-								} else if (command.equals("mkdir")) {
-									//creates a directory in the current directory
-									data = normalIn.readLine();
-									if (data.equals("failure")) {
-										System.out.println("ERROR: Cannot create directory");
-									} 
-								} else if (command.equals("delete")) {
-									//deletes the specified file
-									data = normalIn.readLine();
-									if (data.equals("no file")) {
-										System.out.println("ERROR: File does not exist");
-									} else if (data.equals("failure")) {
-										System.out.println("ERROR: That is not a file");
-									}
-								} else if (command.equals("quit")) {
-									//close all streams and exit
-									normalIn.close();
-									terminateIn.close();
-									normalOut.close();
-									terminateOut.close();
-									stdIn.close();
-									System.exit(0);
-								}
-						}
+						sendCommand(userInput);
 						break;
-						default:
+					default:
 						System.err.println("error parsing input");
 						System.exit(1);
 						break;
